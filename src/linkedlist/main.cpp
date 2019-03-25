adf7b75b87c4efc6970c88a17ba9131678189742
Antoine Salliou <asalliou@tp.michelin.com>
2019-03-25 21:03:32 +0100

    Implement basic utility functions

--- src/linkedlist/main.cpp
+++ src/linkedlist/main.cpp
@@ -2,5 +2,16 @@
 #include "node.h"
 
 int main() {
+    struct node *head = NULL;
+
+    head = insert(1, head);
+    head = insert(2, head);
+    head = insert(3, head);
+    head = insert(9, head);
+    
+    print(head);
+
+    std::cout << "Count: " << length(head) << std::endl;
+
     return 0;
 }
--- src/linkedlist/node.h
+++ src/linkedlist/node.h
@@ -1,8 +1,34 @@
 #pragma once
 
 #include <string>
+#include <iostream>
 
-struct Node {
-  int value;
-  struct Node *next;
+struct node {
+  int data;
+  struct node *next;
 };
+
+struct node* insert(int data, struct node *next) {
+  struct node *new_node = (struct node *)malloc(sizeof(node));
+  new_node->data = data;
+  new_node->next = next;
+  return new_node;
+}
+
+void print(struct node *head) {
+  struct node *current = head;
+  while(current != NULL) {
+    std::cout << "Data: " << current->data << std::endl;
+    current = current->next;
+  }
+}
+
+int length(struct node *head) {
+  struct node *current = head;
+  int count = 0;
+  while(current != NULL) {
+    count++;
+    current = current->next;
+  }
+  return count;
+}
